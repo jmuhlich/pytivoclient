46f8ca964a3ddfac743a8050c66c94c46d93941b
Jeremy Muhlich <jmuhlich@bitflood.org>
2014-11-21 22:54:21 -0500

    do away with command hiding -- too fraught

--- pytivoclient/main.py
+++ pytivoclient/main.py
@@ -14,7 +14,6 @@ class ClientApp(App):
             description='pytivoclient app',
             version='0.1',
             command_manager=CommandManager('pytivoclient.app'),
-            interactive_app_factory=InteractiveApp,
             )
 
     def build_option_parser(self, description, version, argparse_kwargs=None):
@@ -57,41 +56,6 @@ class List(Lister):
         return (('Title', 'Type'),
                 ((i.title, i.content_type) for i in self.app.client.list())
                 )
-
-
-
-# Here we jump through some hoops to disable most of the build-in cmd and cmd2
-# command handlers. They are really overkill for this little app. There is no
-# mechanism to disable an existing command, but a Cmd subclass with the
-# following two features appears to do the trick:
-# 1. Override Cmd.get_names to filter out the unwanted do_* method names. This
-#    hides the commands from help output and tab completion lists.
-# 2. Assign an empty property() to the actual do_* attributes of the class. An
-#    empty property() raises an AttributeError on access, which mimics a
-#    nonexistent method so that the commands can't actually be called. (Cmd is
-#    not a new-style class so __getattribute__ is not an option.) Is there any
-#    other way for a subclass to hide its ancestors methods from them?
-
-# List of commands to disable.
-_commands_to_discard = ('cmdenvironment edit hi history l list pause r save '
-                        'shell show ed li load py run set shortcuts q quit'
-                        ).split(' ')
-# Prefix with 'do_' and store as a set for efficient membership testing.
-_discard_methods = set('do_' + n for n in _commands_to_discard)
-
-class InteractiveApp(cliff.interactive.InteractiveApp):
-    # Feature #1 described above.
-    def get_names(self):
-        names = cliff.interactive.InteractiveApp.get_names(self)
-        names = [n for n in names if n not in _discard_methods]
-        return names
-
-# Feature #2. This is done outside the class definition so that we can just
-# iterate over the set of method names we already have, and use setattr. Could
-# probably achieve this with a metaclass but this is simple and clear.
-for name in _discard_methods:
-    setattr(InteractiveApp, name, property())
-
 
 
 def _find_parser_argument(parser, option):
